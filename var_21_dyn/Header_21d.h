3f1c7c4d285c2f4e4dbe08164934dce3bf67688c
sanyaVsanyah <sanyakindr@gamil.com>
2025-04-03 22:50:13 +0300

    Вариант 21(динамический)

---
+++ var_21_dyn/Header_21d.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef HEADER_21D_H
+#define HEADER_21D_H
+
+#include<iostream>
+
+
+void EnterChoice(int32_t&);
+void RandomFillingArray(float*, int32_t&);
+void FillingArray(float*, int32_t&);
+void EnterSize(int32_t&);
+int32_t CountUniqueElements(float*, int32_t);
+void FindPlusElements(float*, int32_t, int32_t&, int32_t&);
+void SummOfElements(float*, int32_t, int32_t);
+void EnterA(int32_t&);
+void EnterB(int32_t&);
+void AnotherArray(float*, int32_t, int32_t, int32_t);
+
+#endif
