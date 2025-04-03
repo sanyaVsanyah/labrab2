8a05d707340c874a7c6397cbc489928a4d18d3e6
sanyaVsanyah <sanyakindr@gamil.com>
2025-04-03 22:58:35 +0300

    Вариант 6 (динамический)

---
+++ var_6_dyn/Header_6d.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef HEADER_6D_H
+#define HEADER_6D_H
+
+#include <iostream>
+
+void EnterChoice(int32_t&);
+void RandomFillingArray(float*, int32_t&);
+void FillingArray(float*, int32_t&);
+void EnterSize(int32_t&);
+void FindMinimalIndex(float*, int32_t);
+void SummOfElements(float*, int32_t);
+void EnterNumberP(int32_t&);
+void CompressArray(float*, int32_t, int32_t);
+
+#endif
---
+++ var_6_dyn/main_6d.cpp
@@ -0,0 +1,40 @@
+#include "Header_6d.h"
+
+int main()
+{
+
+    int32_t choice{};
+    int32_t size{};
+    float* array = new float[size];
+    int32_t P{};
+
+    EnterChoice(choice);
+    switch (choice)
+    {
+    case 0:
+        RandomFillingArray(array, size);
+        break;
+    case 1:
+        EnterSize(size);
+        break;
+    default:
+        std::cout << "Invalid number.";
+        return 1;
+    }
+    try {
+        if (size <= 0)
+        {
+            throw std::invalid_argument("Wrong size.");
+        }
+
+        FillingArray(array, size);
+        FindMinimalIndex(array, size);
+        SummOfElements(array, size);
+        EnterNumberP(P);
+        CompressArray(array, size, P);
+    }
+    catch (std::invalid_argument e) {
+        std::cout << e.what();
+    }
+    return 0;
+}
